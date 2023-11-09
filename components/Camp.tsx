c149b48ca0c98ff6d61319e4c8148a8a669cf79b
admin <admin@petalslogistics.com>
2023-11-09 21:34:45 +0000

    initial commit

---
+++ components/Camp.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Camp = () => {
+  return (
+    <section className="border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py:20 xl:mb-20">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[600px]"></div>
+    </section>
+  );
+};
+
+export default Camp;
---
+++ components/GetApp.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GetApp = () => {
+  return <div>Get App</div>;
+};
+
+export default GetApp;
