98e776e52529b6c2ea08cde246fd0c0f6f572773
admin <admin@petalslogistics.com>
2023-11-17 00:22:24 +0000

    camp update

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,9 +1,38 @@
 import React from "react";
 
+interface CampProsps {
+  backgroundImage: string;
+  title: string;
+  subtitle: string;
+  peopleJoined: string;
+}
+const CampSite = ({
+  backgroundImage,
+  title,
+  subtitle,
+  peopleJoined,
+}: CampProps) => {
+  return (
+    <div
+      className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
+    >
+      <div className="flex h-full flex-col items-start justify-between p-6"></div>
+    </div>
+  );
+};
+
 const Camp = () => {
   return (
     <section className="border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py:20 xl:mb-20">
-      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[600px]"></div>
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+        <CampSite
+          backgroundImage="bg-bg-img-1"
+          tittle="Putuk Truno Camp"
+          subitle="Prigen, Pasuruan"
+          peopleJoined="50+ Joined"
+        />
+        <CampSite />
+      </div>
     </section>
   );
 };
